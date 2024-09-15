e93662095fd995b80248bc46ec54888a1f50fd39
DevTausif <kabirtausif610@gmail.com>
2024-09-16 05:55:59 +0600

    append js

---
+++ DOM/Get-ElementbyId/append.js
@@ -0,0 +1,40 @@
+const placeList = document.getElementById('places-list');
+const li = document.createElement('li');
+li.innerText = 'Sylhet';
+placeList.appendChild(li);
+
+const mainContainer = document.getElementById('main-container');
+
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML= `
+    <h1>dress section</h1>
+    <ul>
+        <li>T-Shirt</li>
+        <li>Jeans</li>
+        <li>Genji</li>
+    </ul>
+`
+mainContainer.appendChild(sectionDress)
+const styleSection=document
+
+// const sectionNew = document.createElement('section');
+// const h1 = document.createElement('h1');
+// h1.innerText= 'My food list';
+// const ul = document.createElement('ul');
+// const li1 = document.createElement('li');
+// li1.innerText = 'biriani';
+// ul.appendChild(li1);
+
+// const li2 = document.createElement('li');
+// li2.innerText = 'borhani';
+// ul.appendChild(li2);
+
+// const li = document.createElement('li');
+// li3.innerText = 'salad';
+// ul.appendChild(li3);
+
+// section.appendChild(ul)
+// mainContainer.appendChild(sectionNew)
+
+
+// set innerHTML directly
