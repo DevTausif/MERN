d1e2a529e5ab0843e2b783c9972ab95021e6d2d8
DevTausif <kabirtausif610@gmail.com>
2024-09-15 22:28:06 +0600

    get element by id

---
+++ DOM/Get-ElementbyId/get.js
@@ -0,0 +1,7 @@
+const list = document.getElementsByClassName('important-places');
+for(const imp  of list){
+    console.log(imp.innerText)
+}
+
+const fruitsTitle =document.getElementById('fruits-title');
+fruitsTitle.innerText = 'Fruits Description';
